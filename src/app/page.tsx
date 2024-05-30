066386edcb337a46c4b3f10836e25dbda06e0bed
naveenbishwas <naveenbishwas4@gmail.com>
2024-05-30 17:24:03 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import { Main } from "next/document";
+import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructor from "@/components/Instructor";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructor />
+      <Footer />
+    </main>
+  );
+}
---
+++ src/components/ui/3d-card.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const card = () => {
+  return <div>3d-card</div>;
+};
+
+export default card;
